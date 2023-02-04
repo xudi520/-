399ee5862646798cbc12d96d96f253a93851462d
goinatoneearandoutattheother <x15037695173@163.com>
2023-02-04 20:55:36 +0800

    人资项目首次提交

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+// 项目信息配置
+module.exports = {
+  // 项目头部标题Vue Admin Template
+  title: '人力资源',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 头部固定
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // logo设置
+  sidebarLogo: true,
+}
